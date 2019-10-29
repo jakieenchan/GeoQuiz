d1d37e4b770b54d9246f28279feae56b559b9f67
chen <1054636188@qq.com>
2019-10-29 17:12:56 +0800

    just for more questions,imagebutons,click text to next question,

--- app/src/main/java/com/example/jakieenchan/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/jakieenchan/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.jakieenchan.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -10,6 +11,8 @@ import android.widget.Toast;
 
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static  final String TAG = "QuizActivity";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -55,6 +58,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         //引用TextView，将文本内容设置为当前数组索引执行的问题
@@ -117,4 +121,40 @@ public class QuizActivity extends AppCompatActivity {
         });
         updateQuestion();
     }
+
+    //启动一个activty，处于停止状态，不可见
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+
+    //离开前台，暂停activity，用户可见，
+    @Override
+    public  void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    //运行activity，在前台，可见
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+
+    //从暂停变成停止，从可见变成不可见
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+
+    //完成/销毁,activity不存在
+    @Override
+    public  void  onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
+    }
+
 }
